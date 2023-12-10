1a7550a8d7016107d9cdc85e4208190dc489ef14
EntchenEric <eseidel2004@gmail.com>
2023-12-10 11:28:34 +0100

    Added JavaDoc to Helper Class.

--- src/main/java/tanjun/utilitys/Helper.java
+++ src/main/java/tanjun/utilitys/Helper.java
@@ -6,11 +6,20 @@ import java.awt.*;
 import java.util.Random;
 
 public class Helper {
+  /**
+   * Chooses a random entry of a String Array.
+   * @param array The Array containing the Strings from which a random string shall be chosen.
+   * @return a random entry of the Array.
+   */
   public static String getRandom(String[] array) {
     int rnd = new Random().nextInt(array.length);
     return array[rnd];
   }
 
+  /**
+   * Creates the default Embed builder. Used so that every Embed looks the same e.g. have the same color.
+   * @return an EmbedBuilder with the correct base settings.
+   */
   public static EmbedBuilder defaultEmbed(){
     EmbedBuilder embed = new EmbedBuilder();
     embed.setColor(new Color(203, 51, 245));
