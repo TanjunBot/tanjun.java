fa225b301847a08789e95b26a3548fc1dc32a62e
EntchenEric <eseidel2004@gmail.com>
2023-12-10 09:18:56 +0100

    Added first 2 Fun commands

--- src/main/java/tanjun/Main.java
+++ src/main/java/tanjun/Main.java
@@ -4,20 +4,24 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import tanjun.commands.FunCommands;
 import tanjun.commands.UtilityCommands;
 import tanjun.utilitys.Logger;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
-  public static void main(String[] args) throws Exception{
+  public static void main(String[] args) throws Exception {
     Dotenv dotenv = Dotenv.load();
     String token = dotenv.get("BotToken");
 
     JDA jda = JDABuilder.createDefault(token)
             .addEventListeners(new UtilityCommands())
+            .addEventListeners(new FunCommands())
             .setActivity(Activity.playing("Tanjun"))
             .build();
 
@@ -26,10 +30,16 @@ public class Main {
     jda.updateCommands().addCommands(
             Commands.slash("ping", "Calculate ping of the bot"),
             Commands.slash("ban", "Ban a user from the server")
-                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS)) // only usable with ban permissions
+                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.BAN_MEMBERS))
                     .setGuildOnly(true) // Ban command only works inside a guild
-                    .addOption(OptionType.USER, "user", "The user to ban", true) // required option of type user (target to ban)
-                    .addOption(OptionType.STRING, "reason", "The ban reason") // optional reason
+                    .addOption(OptionType.USER, "user", "The user to ban", true)
+                    .addOption(OptionType.STRING, "reason", "The ban reason"),
+            Commands.slash("fun", "commands to interact with Users")
+                    .addSubcommands(
+                            new SubcommandData("hug", "hug a User")
+                                    .addOption(OptionType.USER, "user", "The user to hug", true)
+                                    .addOption(OptionType.STRING, "message", "A Additional Message you want to Send.", false)
+                    )
     ).queue();
 
     Logger.addLog("Slash Commands Initiated", "System");
---
+++ src/main/java/tanjun/commands/FunCommands.java
@@ -0,0 +1,81 @@
+package tanjun.commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import tanjun.utilitys.Helper;
+import tanjun.utilitys.Logger;
+import tanjun.utilitys.TenorApiWrapper;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class FunCommands extends ListenerAdapter {
+  String[] huggingGifs = TenorApiWrapper.GetUrls("hug", 15);
+  String[] eatGifs = TenorApiWrapper.GetUrls("eat", 15);
+  String[] waveGifs = TenorApiWrapper.GetUrls("wave", 15);
+  String[] kissGifs = TenorApiWrapper.GetUrls("kiss", 15);
+  String[] patGifs = TenorApiWrapper.GetUrls("patpat", 15);
+  String[] slapGifs = TenorApiWrapper.GetUrls("slap", 15);
+  String[] boopGifs = TenorApiWrapper.GetUrls("boop", 15);
+  String[] cryGifs = TenorApiWrapper.GetUrls("cry", 15);
+  String[] highfiveGifs = TenorApiWrapper.GetUrls("highFive", 15);
+
+  @Override
+  public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+    String eventName = event.getName();
+    String subcommandName = event.getSubcommandName();
+    switch (eventName) {
+      case "fun":
+        switch (subcommandName) {
+          case "hug":
+          case "eat":
+            event.deferReply().queue();
+            User target = event.getOption("user", OptionMapping::getAsUser);
+            String message = event.getOption("message", OptionMapping::getAsString);
+            EmbedBuilder embed = Helper.defaultEmbed();
+            assert target != null;
+            String url = "https://placehold.co/600x400?text=404";
+            switch (subcommandName) {
+              case "hug":
+                try {
+                  Logger.addLog("hug command was run.", event.getUser().getId());
+                } catch (IOException e) {
+                  throw new RuntimeException(e);
+                }
+                url = Helper.getRandom(huggingGifs);
+                embed.setTitle(event.getUser().getName() + " is hugging " + target.getName());
+                break;
+              case "eat":
+                try {
+                  Logger.addLog("eat command was run.", event.getUser().getId());
+                } catch (IOException e) {
+                  throw new RuntimeException(e);
+                }
+                url = Helper.getRandom(eatGifs);
+                embed.setTitle(event.getUser().getName() + " is eating " + target.getName());
+                break;
+            }
+
+
+            embed.setDescription(message);
+            embed.setImage(url);
+            event.getHook().editOriginalEmbeds(embed.build()).queue();
+
+            break;
+          case null:
+            try {
+              Logger.addLog("Unknown Command " + eventName + " was run.", event.getUser().getId());
+            } catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+            break;
+          default:
+            throw new IllegalStateException("Unexpected value: " + subcommandName);
+        }
+    }
+  }
+}
