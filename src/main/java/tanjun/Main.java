6eacf01e4d8b28b8ee80eed7eec46d4a91c215be
EntchenEric <eseidel2004@gmail.com>
2025-02-27 23:41:20 +0100

    fix math not being listener

--- src/main/java/tanjun/Main.java
+++ src/main/java/tanjun/Main.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 import tanjun.commands.Utility;
+import tanjun.commands.Math;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
@@ -27,6 +28,7 @@ public class Main {
     Tanjun tanjun = new Tanjun(jda, databaseUrl, databaseUsername, databasePassword);
 
     jda.addEventListener(new Utility(tanjun));
+    jda.addEventListener(new Math(tanjun));
 
     tanjun.addLog("System", "Bot Started.");
     tanjun.addLog("System", "Initializing slash commands...");
@@ -52,6 +54,24 @@ public class Main {
                                             new OptionData(OptionType.INTEGER, "n",
                                                     "The number that you want to calculate the Faculty of. e.g. n!",
                                                     true).setRequiredRange(0, 100)
+                                    ),
+                            new SubcommandData("calc", "Calculate a expression")
+                                    .addOptions(
+                                            new OptionData(
+                                                    OptionType.STRING,
+                                                    "expression",
+                                                    "The Expression you want to calculate",
+                                                    true
+                                            ).setRequiredLength(1, 100)
+                                    ),
+                            new SubcommandData("calculator", "Opens the Calculator")
+                                    .addOptions(
+                                            new OptionData(
+                                                    OptionType.STRING,
+                                                    "expression",
+                                                    "The Expression you want to start the Calculator with",
+                                                    false
+                                            ).setRequiredLength(1, 100)
                                     )
                     )
     ).queue();
